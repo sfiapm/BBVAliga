11ab4b16b4cd8d2aafa54ebf20f072070b5a7f30
sfiapm <sofiapineda764@gmail.com>
2023-10-23 01:25:15 -0600

    original jugador

--- TablasBD/tablaJugLiga.php
+++ TablasBD/tablaJugLiga.php
@@ -1,76 +1,84 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JugLiga</title>
-    <link rel="stylesheet" href="CSS/Normalize.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="CSS/Style.css">
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
 </head>
 <body>
 
-<h2>Eliminar Datos de Jugador</h2>
-<form action="procesoEliminar.php" method="post">
-    <label for="jugadorID_eliminar">JugadorID a Eliminar:</label><br>
-    <input type="text" id="jugadorID_eliminar" name="jugadorID_eliminar"><br><br>
-    <input type="submit" value="Eliminar Datos">
-</form>
+<h2>Jugadores de la Liga</h2>
+<?php
 
-<h2>Ingresar Datos de Jugador</h2>
-    <form action="procesoInsertar .php" method="post">
-        <label for="nombre">Nombre:</label><br>
-        <input type="text" id="nombre" name="nombre"><br>
-        <label for="apell_pat">Apellido Paterno:</label><br>
-        <input type="text" id="apellidoPaterno" name="apellidoPaterno"><br>
-        <label for="apell_mat">Apellido Materno:</label><br>
-        <input type="text" id="apellidoMaterno" name="apellidoMaterno"><br>
-        <label for="lugar_nacimiento">Lugar de Nacimiento:</label><br>
-        <input type="text" id="lugarNacimiento" name="lugarNacimiento"><br>
-        <label for="lugar_nacimiento">Apodo:</label><br>
-        <input type="text" id="apodo" name="apodo"><br>
-        <label for="no_playera">Número de Camiseta:</label><br>
-        <input type="text" id="numeroCamiseta" name="numeroCamiseta"><br>
-        <label for="posicion_c">Posición:</label><br>
-        <input type="text" id="posicion" name="posicion"><br>
-        <label for="edad">Edad:</label><br>
-        <input type="text" id="edad" name="edad"><br>
-        <label for="precio_jug">Precio de Transferencia:</label><br>
-        <input type="text" id="precioTransferencia" name="precioTransferencia"><br>
-        <label for="ID_equipo">ID de Equipo:</label><br>
-        <input type="text" id="equipoID" name="equipoID"><br><br>
-        <input type="submit" value="Insertar Datos">
-    </form>
+// Conectar a la base de datos Oracle
+$conn = oci_connect("system", "123456789", "localhost/xe");
 
-<?php  
-// crear conexión con Oracle
-$conexion = oci_connect("system", "123456789", "localhost/xe"); 
+// Ejecutar la consulta SQL
+$sql = 'SELECT * FROM JugLiga';
+$stmt = oci_parse($conn, $sql);
+oci_execute($stmt);
 
-if (!$conexion) {    
-    $m = oci_error();    
-    echo $m['message'], "\n";    
-    exit; 
-} 
-
-// Mostrar los datos de la tabla 'Jugadores'
-$stid = oci_parse($conexion, 'SELECT * FROM NewBDLIGA.JugLiga');
-oci_execute($stid);
-
-echo "<h2>Datos de la tabla Jugadores:</h2>";
-echo "<table>";
-while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) {
-    echo "<tr>";
-    foreach ($row as $item) {
-        echo "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>";
-    }
-    echo "</tr>";
+// Obtener los resultados de la consulta
+$rows = [];
+while ($row = oci_fetch_array($stmt, OCI_ASSOC)) {
+  $rows[] = $row;
 }
-echo "</table>";
 
-oci_free_statement($stid);
-oci_close($conexion);
+// Cerrar la conexión a la base de datos
+oci_close($conn);
+
 ?>
+
+
+<table>
+  <tr>
+    <th>ID_Jugador</th>
+    <th>Nombre</th>
+    <th>Apellido Paterno</th>
+    <th>Apellido Materno</th>
+    <th>Edad</th>
+    <th>Posicion</th>
+    <th>Apodo</th>
+    <th>Lugar de Nacimiento</th>
+    <th>No. de Camisa</th>
+    <th>Precio de Mercado (Euros)</th>
+    <th>ID del Director Técnico</th>
+    <th>ID del Equipo</th>
+  </tr>
+  <!-- Aquí se insertarán los datos de la base de datos -->
+  <tbody>
+    <?php foreach ($rows as $row): ?>
+      <tr>
+        <td><?php echo $row['ID_JUGADOR']; ?></td>
+        <td><?php echo $row['NOMBRE']; ?></td>
+        <td><?php echo $row['A_PATERNO']; ?></td>
+        <td><?php echo $row['A_MATERNO']; ?></td>
+        <td><?php echo $row['EDAD']; ?></td>
+        <td><?php echo $row['POSICION']; ?></td>
+        <td><?php echo $row['APODO']; ?></td>
+        <td><?php echo $row['LUGARNACIMIENTO']; ?></td>
+        <td><?php echo $row['NO_CAMISA']; ?></td>
+        <td><?php echo $row['PRECIOMERCADO_EUROS']; ?></td>
+        <td><?php echo $row['ID_DT']; ?></td>
+        <td><?php echo $row['ID_EQUIPO']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
 </body>
 </html>
+
+
